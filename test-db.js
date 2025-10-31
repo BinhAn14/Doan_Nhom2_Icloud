5913aeb1a183cd6cf8e3c57b990e0e33031b9678
Cao Nguyên Bình An <an100277@donga.edu.vn>
2025-10-31 16:39:18 +0700

    Create test-db.js

---
+++ test-db.js
@@ -0,0 +1,13 @@
+const { getPool } = require("./db");
+
+async function testConnection() {
+  try {
+    const pool = getPool();
+    const [rows] = await pool.query("SELECT 1");
+    console.log("✅ MySQL connected!", rows);
+  } catch (err) {
+    console.error("❌ MySQL connection error:", err);
+  }
+}
+
+testConnection();
